347d0e78428ec039f33e137fab4c2a86e78d065d
arzedlab <arzedlab@gmail.com>
2021-09-10 15:21:31 +0500

    Added 704th Task | Leetcode.com

--- binary_search/problems/704.py
+++ binary_search/problems/704.py
@@ -5,7 +5,6 @@
 # If target exists, then return its index. Otherwise, return -1.
 # You must write an algorithm with O(log n) runtime complexity.
 from typing import List
-
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         low = 0
@@ -25,3 +24,4 @@ class Solution:
                 else:
                     low = mid + 1
             return -1
+            
