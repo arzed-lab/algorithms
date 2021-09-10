c7dae20073a7bce55bf6215856ec58b915c0c649
arzedlab <arzedlab@gmail.com>
2021-09-10 14:44:31 +0500

    Added 704 Task | Leetcode

---
+++ binary_search/problems/704.py
@@ -0,0 +1,27 @@
+# 704. Binary Search | Leetcode.com
+
+# Given an array of integers nums which is sorted in ascending order,
+# and an integer target, write a function to search target in nums. 
+# If target exists, then return its index. Otherwise, return -1.
+# You must write an algorithm with O(log n) runtime complexity.
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        
+        if nums[0] == target:
+            return 0
+        else:
+            while low <= high:
+                mid = (low + high) // 2
+                guess = nums[mid]
+
+                if guess == target: 
+                    return mid
+                elif guess > target:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return -1
